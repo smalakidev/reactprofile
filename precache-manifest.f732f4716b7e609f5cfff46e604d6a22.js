037feeed6caa6e080bfa8fb2a59288425621573c
Santosh <santoshmalaki@gmail.com>
2021-05-11 00:15:08 -0400

    Updates

---
+++ precache-manifest.f732f4716b7e609f5cfff46e604d6a22.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f067f4ef0f9ef68a75fd4f22d5a3d406",
+    "url": "/reactprofile/index.html"
+  },
+  {
+    "revision": "6edec0987f6ac9e8db5b",
+    "url": "/reactprofile/static/css/main.95f58303.chunk.css"
+  },
+  {
+    "revision": "ef02ea6d01ef814393ad",
+    "url": "/reactprofile/static/js/2.b06002cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactprofile/static/js/2.b06002cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6edec0987f6ac9e8db5b",
+    "url": "/reactprofile/static/js/main.9c7d6170.chunk.js"
+  },
+  {
+    "revision": "8bd621b8acb7c080f9fa",
+    "url": "/reactprofile/static/js/runtime-main.0baf2285.js"
+  },
+  {
+    "revision": "949399b3813c8d705de2c366295b9ed7",
+    "url": "/reactprofile/static/media/pic1.949399b3.png"
+  },
+  {
+    "revision": "265a008129259b21a684ca32dc636218",
+    "url": "/reactprofile/static/media/pic2.265a0081.png"
+  },
+  {
+    "revision": "b4f12242c87f97dc678a85b7a663fa84",
+    "url": "/reactprofile/static/media/pic3.b4f12242.png"
+  },
+  {
+    "revision": "d619e6ef485049c135273e7dfb74f46a",
+    "url": "/reactprofile/static/media/pic4.d619e6ef.png"
+  },
+  {
+    "revision": "a8d8baefa9ba0fd9ba920c03119fcb73",
+    "url": "/reactprofile/static/media/selfie.a8d8baef.png"
+  }
+]);
