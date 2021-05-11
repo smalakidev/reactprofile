7e13375246ab9f8f0247b54c7a0756a26f73b73a
Santosh <santoshmalaki@gmail.com>
2021-05-10 21:14:45 -0400

    made edits

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import self from '../img/selfie.png';
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
 
 export let colors = ["rgb(185, 85, 211)", "rgb(247, 10, 7)"];
 /*
@@ -14,15 +12,14 @@ the site.
 
 
 /*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+ use "npm start" to get an idea
 of what each of the values mean.
  */
 
 export const info = {
     firstName: "Santosh",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    lastName: "Malaki",
+    initials: "SM", // the example uses first and last, but feel free to use three or more if you like.
     position: "Junior Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self portrait in the "img" folder something else!
     gradient: `linear-gradient(135deg, ${colors})`, // don't change this either
@@ -36,10 +33,9 @@ export const info = {
         ["https://facebook.com/", facebookIcon()], // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
         ["https://instagram.com/", instagramIcon()], // Just change the links so that they lead to your social profiles.
         ["https://linkedin.com", linkedInIcon()],
-        ["https://medium.com", mediumIcon()],
-        ["https://twitter.com", twitterIcon()]
+
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Santosh! I'm a current graduate from the JHU Bootcamp by Trilogy Services, and I'd be delighted to be both interviewed and hired by you ",
     hobbies: [
         ["📖", "reading"], // Same as above, change the emojis to match / relate to your hobbies or interests.
         ["🎭", "theater"], // You can also remove the emojis if you'd like, I just think they look cute :P
