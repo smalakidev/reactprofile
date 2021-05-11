12724f471e05a060fba0d16d4214c681036efd43
Santosh <santoshmalaki@gmail.com>
2021-05-10 23:18:06 -0400

    Did some debugging

---
+++ build/precache-manifest.4e76ed486c2bda13e479981096d4abba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b770f5886db56c80513f36f0e3cfad5",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "79506c3056c8113c1543",
+    "url": "/myportfolio/static/css/main.95f58303.chunk.css"
+  },
+  {
+    "revision": "ef02ea6d01ef814393ad",
+    "url": "/myportfolio/static/js/2.b06002cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myportfolio/static/js/2.b06002cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79506c3056c8113c1543",
+    "url": "/myportfolio/static/js/main.f66740f3.chunk.js"
+  },
+  {
+    "revision": "27ff665cd96372b8b66a",
+    "url": "/myportfolio/static/js/runtime-main.047ac6ea.js"
+  },
+  {
+    "revision": "949399b3813c8d705de2c366295b9ed7",
+    "url": "/myportfolio/static/media/pic1.949399b3.png"
+  },
+  {
+    "revision": "265a008129259b21a684ca32dc636218",
+    "url": "/myportfolio/static/media/pic2.265a0081.png"
+  },
+  {
+    "revision": "b4f12242c87f97dc678a85b7a663fa84",
+    "url": "/myportfolio/static/media/pic3.b4f12242.png"
+  },
+  {
+    "revision": "d619e6ef485049c135273e7dfb74f46a",
+    "url": "/myportfolio/static/media/pic4.d619e6ef.png"
+  },
+  {
+    "revision": "a8d8baefa9ba0fd9ba920c03119fcb73",
+    "url": "/myportfolio/static/media/selfie.a8d8baef.png"
+  }
+]);
